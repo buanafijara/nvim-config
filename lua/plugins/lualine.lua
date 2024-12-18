c8d3ca854658a97655749daf7e8a133b382b79ca
adjie_macbook <buanafijara@gmail.com>
2024-12-18 07:39:53 +0700

    add buffer in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,23 @@ return {
 			options = {
 				theme = "dracula",
 			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+          {
+            "buffers",
+            show_filename_only = false,
+            buffers_color = {
+              active = { bg = "#24283b", fg = "#c0caf5" },
+              inactive = { bg = "#1f2335", fg = "#3b4261" }
+            },
+          },
+				},
+				lualine_c = { "branch", "diff", "diagnostics" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 		})
 	end,
 }
