ea88d8bddebd41eaa7d096293321ed19154d4a6c
adjie_pancaran <buanafijara@gmail.com>
2024-12-23 09:51:38 +0700

    feat: add auto-session plugin

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  },
+  config = function()
+     require('auto-session').setup({
+  bypass_save_filetypes = { 'alpha', 'dashboard' } -- or whatever dashboard you use
+})
+
+  end
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -33,3 +33,5 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+-- Auto-Session
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
