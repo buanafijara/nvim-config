47d2895bc1a1e1518f114e9ae442c189c90af485
adjie_macbook <buanafijara@gmail.com>
2025-01-06 00:51:53 +0700

    transparent bg

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -39,3 +39,19 @@ vim.o.foldenable = true
 -- Auto-Session
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 -- test commit
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local highlights = {
+      'Normal',
+      'LineNr',
+      'Folded',
+      'NonText',
+      'SpecialKey',
+      'VertSplit',
+      'SignColumn',
+      'EndOfBuffer',
+      'TablineFill', -- this is specific to how I like my tabline to look like
+    }
+    for _, name in pairs(highlights) do vim.cmd.highlight(name .. ' guibg=none ctermbg=none') end 
+end, 
+})
