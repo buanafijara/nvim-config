73fc9a96f5b2d33efe4b37c17d8fe51ef9af8954
adjie_1659 <adjie.buanafijar@pancaran-shipping.co.id>
2025-01-14 10:09:57 +0700

    override catppuccin color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,24 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = false,
+    -- enabled = false,
+    -- lazy = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        color_overrides = {
+          latte = {
+          },
+          frappe = {},
+          macchiato = {},
+          mocha = {
+            surface1 = "#b7dff7"
+          },
+        }
+      })
       vim.cmd.colorscheme "catppuccin-mocha"
+      -- vim.cmd.colorscheme "catppuccin-latte"
     end
   }
 }
